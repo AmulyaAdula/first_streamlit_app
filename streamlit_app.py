04698cc97a6b5324055508d5a2f3282d35d5b569
AmulyaAdula <106382255+AmulyaAdula@users.noreply.github.com>
2022-06-05 01:14:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit'));
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')");
